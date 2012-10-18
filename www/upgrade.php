0b56fe40e8998322c931832378fe4160ceeabed4
Kelvin Mo <kmo@users.sourceforge.net>
2012-10-18 20:11:51 +1100

    Fix for #118: Exit gracefully when configuration file is not found

--- www/upgrade.php
+++ www/upgrade.php
@@ -50,15 +50,17 @@ $upgrade_access_check = TRUE;
 /* ----- Do not modify anything following this line ------------------------- */
 
 include_once "version.inc.php";
+include_once "locale.inc.php";
 if (file_exists("config.php")) {
     include_once "config.php";
 } elseif (file_exists("config.inc")) {
     include_once "config.inc";
     define('UPGRADE_LEGACY_CONFIG_INC', TRUE);
+} else {
+    die(t('No configuration file found.  See the <a href="!url">manual</a> for instructions on how to set up a configuration file.', array('!url' => 'http://simpleid.koinic.net/documentation/getting-started')));
 }
 include_once "config.default.php";
 include_once "log.inc.php";
-include_once "locale.inc.php";
 include_once "common.inc.php";
 include_once "simpleweb.inc.php";
 include_once "openid.inc.php";
@@ -115,11 +117,6 @@ function upgrade_start() {
     $xtpl->assign('css', '@import url(' . get_base_path() . 'html/upgrade.css);');
     $xtpl->assign('footer_doc', t('Documentation'));
     $xtpl->assign('footer_support', t('Support'));
-    
-    // Check if the configuration file has been defined
-    if (!defined('SIMPLEID_BASE_URL')) {
-        indirect_fatal_error(t('No configuration file found.  See the <a href="!url">manual</a> for instructions on how to set up a configuration file.', array('!url' => 'http://simpleid.koinic.net/documentation/getting-started')));
-    }
     
     if (!is_dir(SIMPLEID_IDENTITIES_DIR)) {
         indirect_fatal_error(t('Identities directory not found.  See the <a href="!url">manual</a> for instructions on how to set up SimpleID.', array('!url' => 'http://simpleid.koinic.net/documentation/getting-started')));
