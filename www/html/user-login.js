3a9ef0c183a0b5573986365536dc8543a2b07147
Kelvin Mo <kmo@users.sourceforge.net>
2013-05-02 23:00:34 +1000

    Added GUI for OTP support

--- www/html/user-login.js
+++ www/html/user-login.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     if (!$('.login-security').is('.allow-autocomplete')) $('#edit-pass').attr('autocomplete', 'off');
     
     if ($('#edit-name').is('.form-text')) $('#edit-name').focus();
+    if ($('#edit-otp').is('.form-text')) $('#edit-otp').focus();
 });
 
