795d93678bc2fbdedf490fc21186cf41afe51756
Kelvin Mo <kmo@users.sourceforge.net>
2013-05-09 00:16:51 +1000

    Enhanced documentation for OTP functions

--- www/user.inc.php
+++ www/user.inc.php
@@ -334,7 +334,32 @@ function user_verify_credentials($uid, $credentials) {
     return false;
 }
 
-
+/**
+ * Verifies a one time password (OTP) specified by the user.
+ *
+ * This function compares an OTP supplied by a user with the OTP
+ * calculated based on the current time and the parameters of the
+ * algorithm.  The parameters, such as the secret key, are supplied
+ * using in $params.  These parameters are typically stored for each
+ * user in the user store.
+ *
+ * To allow for clocks going out of sync, the current time will be
+ * by a number (in time steps) specified in $params['drift'].  If
+ * the OTP supplied by the user is accepted, $params['drift'] will
+ * be also be updated with the latest difference.
+ *
+ * To allow for network delay, the function will accepts OTPs which
+ * is a number of time steps away from the OTP calculated from the
+ * adjusted time.  The maximum number of time steps is specified in
+ * the $max_drift parameter.
+ *
+ * @param array &$params the OTP parameters stored
+ * @param string $code the OTP supplied by the user
+ * @param int $max_drift the maximum drift allowed for network delay, in
+ * time steps
+ * @return bool whether the OTP supplied matches the OTP generated based on
+ * the specified parameters, within the maximum drift
+ */
 function user_verify_otp(&$params, $code, $max_drift = 1) {
     switch ($params['type']) {
         case 'totp':
@@ -439,14 +464,15 @@ function _user_logout() {
 }
 
 /**
- * Displays a user login form.
+ * Displays a user login or a login verification form.
  *
  * @param string $destination the SimpleID location to which the user is directed
  * if login is successful
  * @param string $state the current SimpleID state, if required by the location
  * @param string $fixed_uid the user name to be included in the login form; if NULL, the user
  * is asked to supply the user name.  If $mode is otp this cannot be null
- * @param string $mode either credentials or otp
+ * @param string $mode either credentials (login form) or otp (login verification
+ * form)
  */
 function user_login_form($destination = '', $state = NULL, $fixed_uid = NULL, $mode = 'credentials') {
     global $xtpl;
@@ -527,7 +553,10 @@ function user_login_form($destination = '', $state = NULL, $fixed_uid = NULL, $m
     $xtpl->out('main');
 }
 
-
+/**
+ * Displays the page used to set up login verification using one-time
+ * passwords.
+ */
 function user_otp_page() {
     global $xtpl, $user;
 
@@ -928,6 +957,25 @@ function _user_cookieauth_get_name() {
     return "autologin-" . md5(SIMPLEID_BASE_URL);
 }
 
+/**
+ * Calculates a Time-Based One-Time Password (TOTP) based on RFC 6238.
+ *
+ * This function returns an integer calculated from the TOTP algorithm.
+ * The returned integer may need to be zero-padded to return a string
+ * with the required number of digits
+ *
+ * @param string $secret the shared secret as a binary string
+ * @param int $time the time to use in the HOTP algorithm.  If NULL, the
+ * current time is used
+ * @param int $period the time step in seconds
+ * @param int $drift the number of time steps to be added to the time to
+ * adjust for transmission delay
+ * @param string $algorithm the hashing algorithm as supported by
+ * the hash_hmac() function
+ * @param int $digits the number of digits in the one-time password
+ * @param int the one-time password
+ * @link http://tools.ietf.org/html/rfc6238
+ */
 function user_totp($secret, $time = NULL, $period = 30, $drift = 0, $algorithm = 'sha1', $digits = 6) {
     if ($time == NULL) $time = time();
     $counter = floor($time / $period) + $drift;
@@ -936,7 +984,20 @@ function user_totp($secret, $time = NULL, $period = 30, $drift = 0, $algorithm =
 }
 
 /**
- * @param string $data a 64 bit bigendian number!
+ * Calculates a HMAC-Based One-Time Password (HOTP) based on RFC 4226.
+ *
+ * This function returns an integer calculated from the HOTP algorithm.
+ * The returned integer may need to be zero-padded to return a string
+ * with the required number of digits
+ *
+ * @param string $secret the shared secret as a binary string
+ * @param string $data the counter value as a 64 bit in
+ * big endian encoding
+ * @param string $algorithm the hashing algorithm as supported by
+ * the hash_hmac() function
+ * @param int $digits the number of digits in the one-time password
+ * @param int the one-time password
+ * @link http://tools.ietf.org/html/rfc4226
  */
 function user_hotp($secret, $data, $algorithm = 'sha1', $digits = 6) {
     // unpack produces a 1-based array, we use array_merge to convert it to 0-based
