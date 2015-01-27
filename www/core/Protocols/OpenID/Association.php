88ba06954dce4e69a348f040e9896f021131fe18
Kelvin Mo <kmo@users.sourceforge.net>
2015-01-27 20:32:22 +1100

    Migration of core OpenID functions

---
+++ www/core/Protocols/OpenID/Association.php
@@ -0,0 +1,212 @@
+<?php 
+/*
+ * SimpleID
+ *
+ * Copyright (C) Kelvin Mo 2014
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ */
+
+namespace SimpleID\Protocols\OpenID;
+
+use \SimpleID\Crypt\Random;
+
+
+/**
+ * A class representing an OpenID association.
+ */
+class Association {
+
+    const ASSOCIATION_PRIVATE = 2;
+    const ASSOCIATION_SHARED = 1;
+
+    /** @var string the association handle */
+    private $assoc_handle;
+
+    /** @var string the association type */
+    private $assoc_type;
+
+    /** @var string the MAC key */
+    private $mac_key;
+
+    /** @var int the time the association was created */
+    private $created;
+
+    /** @var bool whether the association created is private under
+     * stateless mode */
+    private $private = false;
+
+
+    /**
+     * Creates an association for OpenID versions 1 and 2.
+     *
+     * This function calls {@link DiffieHellman::associateAsServer()} where required, to 
+     * generate the cryptographic values required for an association response.
+     *
+     * @param int $mode either ASSOCIATION_SHARED or ASSOCIATION_PRIVATE
+     * @param string $assoc_type a valid OpenID association type
+     * @link http://openid.net/specs/openid-authentication-1_1.html#anchor14, http://openid.net/specs/openid-authentication-2_0.html#anchor20
+     */
+    function __construct($mode = self::ASSOCIATION_SHARED, $assoc_type = 'HMAC-SHA1') {
+        $rand = new Random();        
+        $assoc_types = self::getAssociationTypes();
+        
+        $this->assoc_handle = $rand->id();
+
+        $this->assoc_type = $assoc_type;
+        $mac_size = $assoc_types[$assoc_type]['mac_size'];
+        $this->mac_key = base64_encode($rand->bytes($mac_size));
+
+        $this->created = time();
+
+        if ($mode == self::ASSOCIATION_PRIVATE) $this->private = true;
+    }
+
+    /**
+     * Returns the association handle.
+     *
+     * @return string the association handle
+     */
+    function getHandle() {
+        return $this->assoc_handle;
+    }
+
+    /**
+     * Returns the creation time.
+     *
+     * @return int the creation time
+     */
+    function getCreationTime() {
+        return $this->created;
+    }
+
+    /**
+     * Returns whether this is a private association.
+     *
+     * @return bool true if this is a private association
+     */
+    function isPrivate() {
+        return $this->private;
+    }
+
+    /**
+     * Creates data an OpenID association response.
+     *
+     * This function calls {@link SimpleID\Protocols\OpenID\DiffieHellman::assciateAsServer()} where required, to 
+     * generate the cryptographic values required for an association response.
+     *
+     * @param string $session_type a valid OpenID session type
+     * @param string $dh_consumer_public for Diffie-Hellman key exchange, the public key of the relying party encoded in Base64
+     * @param string $dh_modulus for Diffie-Hellman key exchange, the modulus encoded in Base64
+     * @param string $dh_gen for Diffie-Hellman key exchange, g encoded in Base64
+     * @return array data that can be fed into an OpenID association response
+     * @link http://openid.net/specs/openid-authentication-1_1.html#anchor14, http://openid.net/specs/openid-authentication-2_0.html#anchor20
+     */
+    function getOpenIDResponse($session_type = 'no-encryption', $dh_consumer_public = NULL, $dh_modulus = NULL, $dh_gen = NULL) {        
+        $assoc_types = self::getAssociationTypes();
+        $session_types = self::getSessionTypes();
+        
+        $response = array(
+            'assoc_handle' => $this->assoc_handle,
+            'assoc_type' => $this->assoc_type,
+        );
+        
+        // If $session_type is '', then it must be using OpenID 1.1 (blank parameter
+        // is not allowed for OpenID 2.0.  For OpenID 1.1 blank requests, we don't
+        // put a session_type in the response.
+        if ($session_type != '') $response['session_type'] = $session_type;
+        
+        if (($session_type == 'no-encryption') || ($session_type == '')) {
+            $response['mac_key'] = $this->mac_key;
+        } elseif ($session_type == 'DH-SHA1' || $session_type == 'DH-SHA256') {
+            $algo = $session_types[$session_type]['algo'];
+            $dh = new DiffieHellman($dh_modulus, $dh_gen, $algo);
+            
+            $result = $dh->associateAsServer(base64_decode($this->mac_key), $dh_consumer_public);
+            $response['dh_server_public'] = $result['dh_server_public'];
+            $response['enc_mac_key'] = $result['enc_mac_key'];
+        }
+
+        return $response;
+    }
+
+    /**
+     * Calculates a signature of an OpenID message
+     *
+     * @param Message $message the message to sign
+     * @return string the signature encoded in Base64
+     */
+    function sign($message) {
+        $assoc_types = self::getAssociationTypes();
+        $signature = '';
+        $algo = $assoc_types[$this->assoc_type]['algo'];
+        $secret = base64_decode($this->mac_key);
+        $signature = hash_hmac($algo, $message->getSignatureBaseString(), $secret, true);
+
+        return base64_encode($signature);
+    }
+
+    /**
+     * Returns a string representation of the association for debugging purposes
+     *
+     * @return string a string representation of the association
+     */
+    function toString() {
+        return sprintf('private: %1$s, assoc_handle: %2$s, assoc_type: %3$s', $this->private, $this->assoc_handle, $this->assoc_type);
+    }
+
+    /**
+     * Returns the association types supported by this server.
+     *
+     * @return array an array containing the association types supported by this server as keys
+     * and an array containing the key size (mac_size) and HMAC algorithm (algo) as
+     * values
+     */
+    static function getAssociationTypes() {
+        $association_types = array('HMAC-SHA1' => array('mac_size' => 20, 'algo' => 'sha1'));
+        if (in_array('sha256', hash_algos())) $association_types['HMAC-SHA256'] = array('mac_size' => 32, 'algo' => 'sha256');
+        return $association_types;
+    }
+
+    /**
+     * Returns the association types supported by this server and the version of
+     * OpenID.
+     *
+     * OpenID version 1 supports an empty string as the session type.  OpenID version 2
+     * reqires a session type to be sent.
+     *
+     * @param bool $is_https whether the transport layer encryption is used for the current
+     * connection
+     * @param float $version the OpenID version, either OPENID_VERSION_1_1 and OPENID_VERSION_2
+     * @return array an array containing the session types supported by this server as keys
+     * and an array containing the hash function (hash_func) as
+     * values
+     */
+    static function getSessionTypes($is_https = TRUE, $version = Message::OPENID_VERSION_2) {
+        $session_types = array(
+            'DH-SHA1' => array('algo' => 'sha1'),
+        );
+        if (in_array('sha256', hash_algos())) $session_types['DH-SHA256'] = array('algo' => 'sha256');
+        if (($version >= Message::OPENID_VERSION_2) && ($is_https == TRUE)) {
+            // Under OpenID 2.0 no-encryption is only allowed if TLS is used
+            $session_types['no-encryption'] = array();
+        }
+        if ($version == Message::OPENID_VERSION_1_1) $session_types[''] = array();
+        return $session_types;
+    }
+}
+
+?>
---
+++ www/core/Protocols/OpenID/Request.php
@@ -0,0 +1,171 @@
+<?php
+/*
+ * SimpleID
+ *
+ * Copyright (C) Kelvin Mo 2014
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ */
+
+namespace SimpleID\Protocols\OpenID;
+
+/**
+ * Class representing an OpenID request.
+ */
+class Request extends Message {
+
+    /**
+     * Constant for the OP-local identifier which indicates that SimpleID should choose an identifier
+     *
+     * @link http://openid.net/specs/openid-authentication-2_0.html#anchor27
+     */
+    const OPENID_IDENTIFIER_SELECT = 'http://specs.openid.net/auth/2.0/identifier_select';
+
+    /**
+     * Constructs a new OpenID request.
+     *
+     * @param array $request the request in array form
+     */
+    public function __construct($request) {
+        $this->container = $request;
+
+        foreach ($request as $key => $value) {
+            if (strpos($key, 'openid.ns.') === 0) {
+                $alias = substr($key, 10);
+                $this->extension_map[$value] = $alias;
+            }
+        }
+
+        if (!isset($this->container['openid.ns'])) {
+            $this->version = Message::OPENID_VERSION_1_1;
+        } elseif ($this->container['openid.ns'] != Message::OPENID_NS_2_0) {
+            $this->version = Message::OPENID_VERSION_1_1;
+        } else {
+            $this->version = Message::OPENID_VERSION_2;
+        }
+    }
+
+    /**
+     * Gets the realm from the OpenID request.  This is specified differently
+     * depending on the OpenID version.
+     *
+     * @return string the realm URI
+     */
+    public function getRealm() {
+        $version = $this->getVersion();
+
+        if ($version == Message::OPENID_VERSION_1_1) {
+            $realm = $this->container['openid.trust_root'];
+        }
+
+        if ($version == Message::OPENID_VERSION_2) {
+            $realm = $this->container['openid.realm'];
+        }
+        
+        if (!$realm) {
+            $realm = $this->container['openid.return_to'];
+        }
+        
+        return $realm;
+    }
+
+    /**
+     * Determines whether the openid.return_to address matches a realm.
+     *
+     * A URL matches a realm if:
+     *
+     * 1. The URL scheme and port of the URL are identical to those in the realm.
+     *    See RFC 3986, section 3.1 for rules about URI matching.
+     * 2. The URL's path is equal to or a sub-directory of the realm's path.
+     * 3. Either:
+     *    (a) The realm's domain contains the wild-card characters "*.", and the
+     *        trailing part of the URL's domain is identical to the part of the
+     *        realm following the "*." wildcard, or
+     *    (b) The URL's domain is identical to the realm's domain
+     *
+     * @param string $url to URL to test
+     * @param string $realm the realm
+     * @return bool true if the URL matches the realm
+     * @since 0.6
+     */
+    function returnToMatches($realm) {
+        $url = parse_url($this->container['openid.return_to']);
+        $realm = parse_url($realm);
+        
+        foreach(array('user', 'pass', 'fragment') as $key) {
+            if (array_key_exists($key, $url) || array_key_exists($key, $realm))
+                return false;
+        }
+        
+        if ($url['scheme'] != $realm['scheme']) return false;
+        
+        if (!isset($url['port']))
+            $url['port'] = '';
+        if (!isset($realm['port']))
+            $realm['port'] = '';
+        if (($url['port'] != $realm['port']))
+            return false;
+        
+        if (substr($realm['host'], 0, 2) == '*.') {
+            $realm_re = '/^([^.]+\.)?' . preg_quote(substr($realm['host'], 2)) . '$/i';
+        } else {
+            $realm_re = '/^' . preg_quote($realm['host']) . '$/i';
+        }
+        
+        if (!preg_match($realm_re, $url['host'])) return false;
+        
+        if (!isset($url['path']))
+            $url['path'] = '';
+        if (!isset($realm['path']))
+            $realm['path'] = '';
+        if (substr($realm['path'], -1) == '/') $realm['path'] = substr($realm['path'], 0, -1);
+        if (($url['path'] != $realm['path']) && !preg_match('#^' . preg_quote($realm['path']) . '/.*$#', $url['path'])) return false;
+        
+        return true;
+    }
+
+    /**
+     * Calculates the base string from which an OpenID signature is generated.
+     *
+     * @return string the signature base string
+     * @link http://openid.net/specs/openid-authentication-2_0.html#anchor11
+     */
+    public function getSignatureBaseString() {
+        if (!isset($this->container['openid.signed'])) return null;
+        $signed_fields = explode(',', $this->container['openid.signed']);
+        return $this->buildSignatureBaseString($this->signed_fields, 'openid.');
+    }
+
+    /**
+     * Returns the base string from which an OpenID signature is generated
+     *
+     * @return string the base string
+     */
+    protected function getPrefix() {
+        return 'openid.';
+    }
+
+    /**
+     * Returns a string representation of the request.
+     *
+     * @return string
+     */
+    public function toString() {
+        return str_replace(array('+', '%7E'), array('%20', '~'), http_build_query($this->container));
+    }
+}
+
+?>
