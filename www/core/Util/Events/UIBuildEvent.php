57b7e687bad6e7e4b803de669d3b039c64b62b44
Kelvin Mo <kmo@users.sourceforge.net>
2021-07-30 13:13:40 +1000

    Refactor UIBuildEvent into UIBuilder

--- www/core/Util/Events/UIBuildEvent.php
+++ www/core/Util/Events/UIBuildEvent.php
@@ -22,26 +22,16 @@
 
 namespace SimpleID\Util\Events;
 
+use SimpleID\Util\UIBuilder;
+
 /**
- * A generic event used to build user interfaces.
+ * A generic event used to build user interfaces, based
+ * on UIBuilder.
  * 
- * User interfaces built by adding "blocks" using the {@link addBlock()}
- * method.  A block is essentially a piece of HTML code that can be
- * inserted in a particular order.
- * 
- * In addition to blocks, listeners can also add attachments.  Attachments
- * can be things such as CSS style sheets, Javascript references or
- * Javascript code.  Each attachment is associated with a type.
- *
+ * @see SimpleID\Util\UIBuilder
  */
-class UIBuildEvent implements \GenericEventInterface {
+class UIBuildEvent extends UIBuilder implements \GenericEventInterface {
     use GenericEventTrait;
-
-    /** @var array */
-    protected $blocks = [];
-
-    /** @var array */
-    protected $attachments = [];
 
     /**
      * Creates a UI build event
@@ -52,79 +42,6 @@ class UIBuildEvent implements \GenericEventInterface {
         $this->setEventName($eventName);
     }
 
-    /**
-     * Adds a UI block to the event.
-     * 
-     * @param string $id the block ID
-     * @param string $content the contents
-     * @param int $weight the weight
-     * @param array $additional additional data
-     */
-    public function addBlock($id, $content, $weight = 0, $additional = []) {
-        $block = [ 'id' => $id, 'content' => $content, 'weight' => $weight ];
-        $block = array_merge($block, $additional);
-
-        $this->blocks[] = [
-            '#data' => $block,
-            '#weight' => $weight
-        ];
-    }
-
-    /**
-     * Adds an attachment to the event
-     * 
-     * An *attachment* can be a CSS stylesheet or a Javascript file
-     * 
-     * @param string $attachment_type the type of attachment
-     * @param mixed $data the details of the attachment
-     */
-    public function addAttachment($attachment_type, $data) {
-        if (isset($this->attachments[$attachment_type])) {
-            $this->attachments[$attachment_type][] = $data;
-        } else {
-            $this->attachments[$attachment_type] = [ $data ];
-        }
-    }
-
-    /**
-     * Retrieves the blocks from the event, ordered by the
-     * weight, from lowest to highest.
-     * 
-     * @return array
-     */
-    public function getBlocks() {
-        uasort($this->blocks, function($a, $b) { if ($a['#weight'] == $b['#weight']) { return 0; } return ($a['#weight'] < $b['#weight']) ? -1 : 1; });
-        return array_map(function($a) { return $a['#data']; }, $this->blocks);
-    }
-
-    /**
-     * Retrieves all the attachments.
-     * 
-     * This function returns an array of all attachments, with the
-     * key being the attachment type, and the value an array of the
-     * attachment details.
-     * 
-     * Note that the value array may contain duplicates.  To filter
-     * for unique values, use the {@link getAttachmentsByType()} method.
-     * 
-     * @return array the attachments
-     */
-    public function getAttachments() {
-        return $this->attachments;
-    }
-
-    /**
-     * Returns the attachments of a particular type.
-     * 
-     * Only unique elements are returned.
-     * 
-     * @param string $attachment_type the attachment type
-     * @return array
-     */
-    public function getAttachmentsByType($attachment_type) {
-        if (!isset($this->attachments[$attachment_type])) return [];
-        return array_unique($this->attachments[$attachment_type]);
-    }
 }
 
 ?>
