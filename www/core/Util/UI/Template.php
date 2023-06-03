65fe9ef0ede49d73f592f0f9295bfb28d991d26a
Kelvin Mo <kmo@users.sourceforge.net>
2023-06-03 11:45:47 +1000

    Add attachment support to Template

---
+++ www/core/Util/UI/Template.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * SimpleID
+ *
+ * Copyright (C) Kelvin Mo 2023
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ */
+
+namespace SimpleID\Util\UI;
+
+use \Template as F3Template;
+
+/**
+ * An extension to the Fat-Free Framework `Template` class to
+ * support attachments.
+ *
+ */
+class Template extends F3Template implements AttachmentManagerInterface {
+    use AttachmentManagerTrait;
+
+    public function __construct() {
+        parent::__construct();
+
+        // $this->attachments comes from AttachmentManagerTrait
+        // $this->fw comes from the Fat-Free View class
+        $this->attachments = &$this->fw->ref('attachments');
+    }
+}
+
+?>
