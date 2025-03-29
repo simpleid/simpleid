bce9afc440df5a7de93ade0e1b3b93a11c36dbfc
Kelvin Mo <kelvinmo@users.noreply.github.com>
2025-03-30 07:54:49 +1100

    Fix static analysis error

--- www/core/Store/DefaultStoreModule.php
+++ www/core/Store/DefaultStoreModule.php
@@ -131,9 +131,8 @@ class DefaultStoreModule extends StoreModule {
         while (($file = readdir($dir)) !== false) {
             $filename = $this->config['identities_dir'] . '/' . $file;
 
-            /** @var non-falsy-string $filename */
             if (is_link($filename) && readlink($filename)) $filename = readlink($filename);
-            if ((filetype($filename) != "file") || (!preg_match('/^(.+)\.user\.yml$/', $file, $matches))) continue;
+            if (($filename === false) || (filetype($filename) != "file") || (!preg_match('/^(.+)\.user\.yml$/', $file, $matches))) continue;
             
             $uid = $matches[1];
             $test_user = $this->readUser($uid);
